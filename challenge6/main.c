00ab8652d4636cd71beac351476b230abc1e9faa
“[Khalid <[khalid.haiddou01@gmail.com]>
2023-09-14 10:53:58 +0100

    challenge6

---
+++ challenge6/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	float R , C;
+	printf("entrer la distance de rayon:");
+	scanf("%f ", &R);
+	C = R*3.14*2;
+	printf("la circonférence est : %f",C);
+	
+}
+
